271cdc20780a24f1f25e1ca205640e300e5d5616
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-07-15 00:22:50 +0800

    add Tree

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -62,10 +62,19 @@ export default defineConfig({
             {
                 text: 'Dynamic Programming',
                 items: [
-                    { text: 'Introduction', link: '/dynamic-programming/dp-intro'},
-                    { text: '509. Fibonacci Numbers', link: '/dynamic-programming/509'},
-                    { text: '376. Wiggle Subsequence', link: '/dynamic-programming/376'},
-                    { text: '5. Longest Palindromic Substring', link: '/dynamic-programming/5'},
+                    { text: 'Introduction', link: '/dynamic-programming/dp-intro' },
+                    { text: 'Fibonacci Numbers', link: '/dynamic-programming/509' },
+                    { text: 'Wiggle Subsequence', link: '/dynamic-programming/376' },
+                    { text: 'Longest Palindromic Substring', link: '/dynamic-programming/5' },
+                ]
+            },
+
+            {
+                text: 'Tree',
+                items: [
+                    { text: 'Invert Binary Tree', link: '/Tree/226' },
+                    { text: 'Construct Binary Tree from Preorder and Inorder Traversal', link: '/Tree/105' },
+                    
                 ]
             }
         ]
