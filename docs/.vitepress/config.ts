ff5e88f8012721d292038870ff9ee7e763d53f0a
Ryan <91534261+ryankert01@users.noreply.github.com>
2022-07-14 16:47:24 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vitepress'
 
+// const ogImage = 'https://vitejs.dev/og-image.png'
+const ogDescription = 'An algorithm website, with leetcode, and with nice comments and answer'
+const ogTitle = 'AlgoKert'
+const ogUrl = 'https://ryankert.cc/AlgoKert'
+
 export default defineConfig({
     lang: 'en-US',
     title: 'AlgoKert',
+    
+    // ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: ogTitle }],
+    // ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { property: 'og:url', content: ogUrl }],
+    ['meta', { property: 'og:description', content: ogDescription }],
+    
     titleTemplate: 'An algorithm website driven by leetcode examples', // otherwise false
     description: 'An algorithm website, with leetcode, and with nice comments and answer',
     appearance: true, // dark mode enabled
@@ -20,13 +33,21 @@ export default defineConfig({
             { text: 'My Blog', link: 'https://blog.ryankert.cc'},
             { text: 'Contribute Guide', link: '/contribute'}
         ],
+        
         socialLinks: [
             { icon: 'github', link: 'https://github.com/ryankert01' },
             //{ icon: 'linkedin', link: 'https://www.linkedin.com/in/ryankert01/'}
         ],
+        
         editLink: {
             pattern: 'https://github.com/ryankert01/AlgoKert/edit/main/docs/:path'
         },
+        
+        footer: {
+          message: 'Released under the MIT License.',
+          copyright: 'Copyright © 2020-present Ryan'
+        },
+        
         sidebar: [
             {
               text: 'Guide',
