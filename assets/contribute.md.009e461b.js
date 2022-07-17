2d9a31d563afe505e0f7a6ed0ebcde236c6552a5
ryankert01 <ryankert01@users.noreply.github.com>
2022-07-17 11:57:04 +0000

    deploy: a4f5374a1d490e879ea3c1c0d80f6a99848c5c83

---
+++ assets/contribute.md.009e461b.js
@@ -0,0 +1,17 @@
+import{_ as s,c as n,o as a,a as e}from"./app.a4283b1c.js";const A=JSON.parse('{"title":"Contribute Guildlines","description":"","frontmatter":{},"headers":[{"level":2,"title":"Modify Request","slug":"modify-request"},{"level":2,"title":"New Post","slug":"new-post"}],"relativePath":"contribute.md","lastUpdated":1658058989000}'),l={name:"contribute.md"},p=e(`<h1 id="contribute-guildlines" tabindex="-1">Contribute Guildlines <a class="header-anchor" href="#contribute-guildlines" aria-hidden="true">#</a></h1><h2 id="modify-request" tabindex="-1">Modify Request <a class="header-anchor" href="#modify-request" aria-hidden="true">#</a></h2><p>If you think there&#39;s something wrong or if there are some explaination is nice to have.</p><p>send us a <code>issue</code> or <code>PR</code>, we will look into it.</p><h2 id="new-post" tabindex="-1">New Post <a class="header-anchor" href="#new-post" aria-hidden="true">#</a></h2><p>If you want to submit a new post, we have a format that is nice to follow.</p><div class="language- line-numbers-mode"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;"># title</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">[title](/* oj link */)</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">## Intuition</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">/* your thinking process */</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">## Code</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">/* your code ac in oj */</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">## Complexity Analysis</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Time:</span></span>
+<span class="line"><span style="color:#A6ACCD;">Space:</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br></div></div><p>If you finish, send us a PR!</p>`,8),i=[p];function o(r,t,c,u,d,b){return a(),n("div",null,i)}var m=s(l,[["render",o]]);export{A as __pageData,m as default};
